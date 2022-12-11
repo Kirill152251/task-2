de3553ae830be12a383f8e5b363d487f46c4c808
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2022-12-11 21:34:57 +0300

    Add test that checks uniqueness of items

--- app/src/test/java/com/task/RandomListServiceTest.kt
+++ app/src/test/java/com/task/RandomListServiceTest.kt
@@ -1,5 +1,6 @@
 package com.task
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
@@ -11,5 +12,15 @@ internal class RandomListServiceTest {
     fun `list size is even`() {
         val list = service.getRandomList()
         assertTrue(list.size % 2 == 0)
+    }
+
+    @Test
+    fun `list contain only unique items`() {
+        val list = service.getRandomList()
+
+        val expected = list.distinct().size
+        val actual = list.size
+
+        assertEquals(expected, actual)
     }
 }
